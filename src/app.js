bbb558ac0a25b95b7e4d1ee70ba553f1697d10e8
ilyapridanov <pkukudmitriykuku@gmail.com>
2024-01-25 21:27:04 +0300

    feat: app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import {RenderPosition} from '../framework/render.js';
-import EventsPresenter from './events-presenter.js';
-import FiltersPresenter from './filters-presenter.js';
-import HeaderInfoPresenter from './header-info-presenter.js';
-import EventPointsModel from '../model/event-points-model.js';
-import EditPointModel from '../model/edit-point-model.js';
-import DestinationModel from '../model/destination-model.js';
-import OffersModel from '../model/offers-model.js';
+import {RenderPosition} from './framework/render.js';
+import EventsPresenter from './presenter/events-presenter.js';
+import FiltersPresenter from './presenter/filters-presenter.js';
+import HeaderInfoPresenter from './presenter/header-info-presenter.js';
+import EventPointsModel from './model/event-points-model.js';
+import EditPointModel from './model/edit-point-model.js';
+import DestinationModel from './model/destination-model.js';
+import OffersModel from './model/offers-model.js';
 
 const tripMainElement = document.querySelector('.trip-main');
 const tripControlsFilters = document.querySelector('.trip-controls__filters');
@@ -27,7 +27,7 @@ const filtersPresenter = new FiltersPresenter({
 });
 const headerInfoPresenter = new HeaderInfoPresenter({container: tripMainElement, place: RenderPosition.AFTERBEGIN});
 
-export default class MainPresenter {
+export default class BigTripApp {
   init() {
     eventsPresenter.init();
     filtersPresenter.init();
