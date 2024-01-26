3d9ae783fdd2ead5a84af916140041c69a174483
ilyapridanov <pkukudmitriykuku@gmail.com>
2024-01-26 08:08:03 +0300

    feat: update method in event-points-model

--- src/model/event-points-model.js
+++ src/model/event-points-model.js
@@ -1,9 +1,15 @@
 import {eventPointsMock} from '../mock/event-points.js';
+import {updatePoint, addPoint, deletePoint} from '../service/mock-service.js';
+import {updateItem} from '../utils.js';
+import Observable from '../framework/observable.js';
 
-export default class EventPointsModel {
+export default class EventPointsModel extends Observable {
   #eventPoints = [];
+  #service = null;
 
-  constructor() {
+  constructor(service) {
+    super();
+    this.#service = service;
     this.#eventPoints = Array.from(eventPointsMock);
   }
 
@@ -13,5 +19,23 @@ export default class EventPointsModel {
 
   getById(id) {
     return this.#eventPoints.find((eventPoints) => eventPoints.id === id) || null;
+  }
+
+  update(updateType, point) {
+    const updatedPoint = updatePoint(point);
+    this.#eventPoints = updateItem(this.#eventPoints, updatedPoint);
+    this._notify(updateType, updatedPoint);
+  }
+
+  add(updateType, point) {
+    const addedPoint = addPoint(point);
+    this.#eventPoints = [this.#eventPoints, addedPoint];
+    this._notify(updateType, addedPoint);
+  }
+
+  delete(updateType, point) {
+    deletePoint(point);
+    this.#eventPoints = this.#eventPoints.filter((item) => item.id !== point.id);
+    this._notify(updateType);
   }
 }
