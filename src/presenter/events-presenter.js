f198138c35acb6bb9a176cff3f36cfba1ef95ee5
ilyapridanov <pkukudmitriykuku@gmail.com>
2024-01-27 17:48:49 +0300

    start remove point edit view

--- src/presenter/events-presenter.js
+++ src/presenter/events-presenter.js
@@ -50,7 +50,8 @@ export default class EventsPresenter {
 
     this.#renderSort();
     this.#renderList();
-    // this.#renderPoints(); сломалась редактирование
+    // this.#renderPoints();
+    // сломалась редактирование
   }
 
   #clearBoard = ({resetSortType = false} = {}) => {
