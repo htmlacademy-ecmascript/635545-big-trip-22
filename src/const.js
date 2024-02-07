cb1d39402f5854c3fad6073cbcd09a6792e8a63a
ilyapridanov <pkukudmitriykuku@gmail.com>
2024-02-07 12:23:23 +0300

    fix: DATE_FORMAT_DAY_MONTH

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 const POINT_TYPE = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const NATURAL_NUMBERS = [1,2,3,4,5,6,7,8,9,0];
-const DATE_FORMAT_DAY_MONTH = 'DD MMM';
+const DATE_FORMAT_DAY_MONTH = 'MMM DD';
 const DATE_FORMAT_YEAR_DAY_MONTH = 'YYYY-MM-DD';
 const DATE_FORMAT_HOURS_MINUTE = 'HH:mm';
 const DATE_FORMAT_YEAR_DAY_MONTH_HOURS_MINUTE = 'DD/MM/YY HH:mm';
